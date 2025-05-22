d7bb9ed7d82bde462fa1590ef48504aea12a2c21
Keithkat64 <keithkat64@gmail.com>
2025-05-22 12:02:11 +1000

    Switch to Gunicorn for production

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
-import os
 
 app = Flask(__name__)
 CORS(app)
@@ -35,7 +34,3 @@ def validate_bid():
 def next_bid():
     data = request.get_json()
     return jsonify({ "nextBid": "stub" })
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
