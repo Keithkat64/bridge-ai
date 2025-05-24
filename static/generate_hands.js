989f814789c307933d7bf1dd49be11b642de72ec
Keithkat64 <keithkat64@gmail.com>
2025-05-24 20:45:39 +1000

    genv10

--- static/generate_hands.js
+++ static/generate_hands.js
@@ -1,4 +1,4 @@
-  function generateHands() {
+    function generateHands() {
   const suits = ["♠", "♥", "♦", "♣"];
   const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
   const deck = [];
