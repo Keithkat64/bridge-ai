76c5295047b5af50384d5fcdfe7314d8e4c0f2aa
Keithkat64 <keithkat64@gmail.com>
2025-05-29 13:06:04 +1000

    updated validate users 1st bid

--- static/validateUser1stBid.js
+++ static/validateUser1stBid.js
@@ -1,23 +1,42 @@
-function validateUser1stBid(userBid, responderHand) {
-    console.log("Validating bid:", userBid);
-    console.log("Responder hand:", responderHand);
+function validateUser1stBid(user1stBid, responderHand) {
+    // Get the number of hearts and spades from responderHand
+    const responderHearts = responderHand["♥"].length;
+    const responderSpades = responderHand["♠"].length;
+
+    // Check if this is a Stayman hand
+    if (responderHearts < 5 && responderSpades < 5) {
+        // Stayman hand
+        if (user1stBid === "2C") {
+            return true;
+        } else {
+            showModal("Keith thinks this is a Stayman hand. Please bid 2C.");
+            return false;
+        }
+    } else {
+        // Check for transfer to spades
+        if (responderSpades > 4) {
+            // Transfer to spades
+            if (user1stBid === "2H") {
+                return true;
+            } else {
+                showModal("With 5+ spades, you should transfer by bidding 2H. Please try again.");
+                return false;
+            }
+        } else if (responderHearts > 4) {
+            // Transfer to hearts
+            if (user1stBid === "2D") {
+                return true;
+            } else {
+                showModal("With 5+ hearts, you should transfer by bidding 2D. Please try again.");
+                return false;
+            }
+        }
+    }
     
-    if (!userBid || typeof userBid !== "string" || userBid.length !== 2) {
-        console.log("Invalid bid format");
-        return false;
-    }
-
-    if (!responderHand || !responderHand["♠"] || !responderHand["♥"]) {
-        console.error("Responder hand is undefined or incomplete:", responderHand);
-        return false;
-    }
-
-    userBid = userBid.toUpperCase();
-    const has4Spades = responderHand["♠"].length >= 4;
-    const has4Hearts = responderHand["♥"].length >= 4;
-
-    console.log("Has 4 spades:", has4Spades);
-    console.log("Has 4 hearts:", has4Hearts);
-
-    return (userBid === "2C" && (has4Spades || has4Hearts));
+    // If we get here, something unexpected happened
+    showModal("Invalid hand configuration");
+    return false;
 }
+
+// Export the function for use in other modules
+window.validateUser1stBid = validateUser1stBid;
